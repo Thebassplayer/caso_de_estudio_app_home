bb8af147b2a6121eead727396619b2d53cf0d561
Roy Lopez <roylopezdev@gmail.com>
2024-03-04 18:59:14 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import casoDeEstudio from "./assets/caso_estudio_app_home.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="full-background">
+      <img src={casoDeEstudio} alt="caso the estudio image" />
+    </div>
+  );
+}
+
+export default App;
